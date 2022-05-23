591b77b123f705f70791ba42897297e30c4f2a54
LAPTOP-SBE1PNGJ\User <justin@made4u.my>
2022-05-23 10:40:33 +0800

    Correction

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,7 +32,7 @@ class UserController extends Controller
      */
     public function create()
     {
-       return redirect()->view('users.create');
+       return view('users.create');
     }
 
     /**
